571747523a76f624d30262fc2acd8743de7eeaf5
EarthWormGym <adam56.beresford@gmail.com>
2023-12-10 14:01:25 +0000

    Testing multiple historical data cards

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
+let count = 9;
+
 app.get('/api/weather/current', async (req, res) => {
   const location = req.query.location;
   try {
@@ -15,7 +17,8 @@ app.get('/api/weather/current', async (req, res) => {
 
 app.get('/api/weather/historical', async (req, res) => {
     try {
-        const response = await axios.get(`http://api.weatherapi.com/v1/history.json?key=45b8474fde374c41ac3134812232811&q=London&dt=2023-12-05`)
+        const response = await axios.get(`http://api.weatherapi.com/v1/history.json?key=45b8474fde374c41ac3134812232811&q=London&dt=2023-12-0${count}`)
+        count--;
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ message: 'An error occurred' });
