302f13d85519984b9754dbd87242e8d100561a9e
EarthWormGym <adam56.beresford@gmail.com>
2025-04-20 10:35:08 +0100

    fish favourites page in progress

--- src/app/navbar/fish-favourites/fish-favourites.component.ts
+++ src/app/navbar/fish-favourites/fish-favourites.component.ts
@@ -1,20 +1,38 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { FishDisplayItem, fishList } from './models/fish-display-item.model';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-fish-favourites',
   templateUrl: './fish-favourites.component.html',
   styleUrl: './fish-favourites.component.scss',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FishFavouritesComponent implements OnInit {
 
+  private destroyRef = inject(DestroyRef);
+
   fishList: FishDisplayItem[] = fishList;
+  fishSelectControl = new FormControl<string[]>([], { nonNullable: true });
+  maxSelection = 4;
   
   ngOnInit(): void {
-    
+    this.fishSelectControl.valueChanges.pipe(
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(selected => {
+      if (selected.length > this.maxSelection) {
+        this.fishSelectControl.setValue(selected.slice(0, this.maxSelection), { emitEvent: false });
+      }
+    });
+  }
+
+  get selectedFish(): string[] {
+    return this.fishSelectControl.value || [];
   }
 
 }
--- src/app/navbar/fish-favourites/models/fish-display-item.model.ts
+++ src/app/navbar/fish-favourites/models/fish-display-item.model.ts
@@ -7,13 +7,13 @@ export const fishList: FishDisplayItem[] = [
   { name: 'Pike', image: 'pike-icon.png' },
   { name: 'Perch', image: 'perch-icon.png' },
   { name: 'Trout', image: 'trout-icon.png' },
-  { name: 'Bass', image: 'bass-icon.png' },
   { name: 'Common Carp', image: 'common-carp-icon.png' },
+  { name: 'Bass', image: 'bass-icon.png' },
   { name: 'Mirror Carp', image: 'mirror-carp-icon.png' },
   { name: 'Barbel', image: 'barbel-icon.png' },
-  { name: 'Salmon', image: 'salmon-icon.png' },
-  { name: 'Catfish', image: 'catfish-icon.png' },
-  { name: 'Chubb', image: 'chubb-icon.png' },
-  { name: 'Sturgeon', image: 'sturgeon-icon.png' },
   { name: 'Zander', image: 'zander-icon.png' },
+  { name: 'Salmon', image: 'salmon-icon.png' },
+  { name: 'Chubb', image: 'chubb-icon.png' },
+  { name: 'Catfish', image: 'catfish-icon.png' },
+  { name: 'Sturgeon', image: 'sturgeon-icon.png' }
 ];
